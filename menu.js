f9ab0ce20f48965a88a3d5dd4f6d6fdfdbb3fdf8
ajs6f4 <31222869+ajs6f4@users.noreply.github.com>
2017-11-09 13:16:27 -0600

    hamburger menu

---
+++ menu.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.menu-trigger').click(function(){
+  $('#nav li').slideToggle(500);
+});
+$(window).resize(function(){
+  if ( $(window).width()>500){
+    $('#nav li').removeAttr('style');
+    }
+  });
+});
